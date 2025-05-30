d3c4b109d57563a0d2892ae88c94160856d0014b
nonmatrix <nonmatrixuniversity@gmail.com>
2025-05-30 18:44:08 +0530

    Initial commit - Movie Tracker Dashboard

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_OSQ6QWZ7WqeINOYQr3M2hgYfO5hNClk",
+    authDomain: "movietracker-23eaa.firebaseapp.com",
+    databaseURL: "https://movietracker-23eaa-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "movietracker-23eaa",
+    storageBucket: "movietracker-23eaa.firebasestorage.app",
+    messagingSenderId: "900963250607",
+    appId: "1:900963250607:web:1e78185bfb5794467dbf5a"
+};
